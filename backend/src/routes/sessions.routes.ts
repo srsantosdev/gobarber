fd0b50565202181fe2532f1d4d22009f4d85c491
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-06-24 20:51:45 -0300

    Autenticação de usuário

---
+++ backend/src/routes/sessions.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionRoutes = Router();
+
+sessionRoutes.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user, token } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user, token });
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default sessionRoutes;
