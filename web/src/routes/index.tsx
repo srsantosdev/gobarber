8a824c96503acc5d8b09792e5fdf66e3b1984f9f
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-07-04 17:28:34 -0300

    Rotas Autenticadas

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   </BrowserRouter>
 );
