fdb30dfd6e8c81daa70d9cee313ebbf871d85a6f
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-07-05 20:13:19 -0300

    Tela de Autenticação

--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
@@ -11,4 +13,31 @@ export const Title = styled.Text`
   color: #f4ebe8;
   font-size: 24px;
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  color: #ff9000;
+  margin-left: 16px;
 `;
