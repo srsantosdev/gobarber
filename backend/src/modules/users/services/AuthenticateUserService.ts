6338023950ffac81f793def5fa18b47b8320e230
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-07-13 23:21:20 -0300

    Criação de Testes

--- backend/src/modules/users/services/AuthenticateUserService.ts
+++ backend/src/modules/users/services/AuthenticateUserService.ts
@@ -1,4 +1,3 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
 import { inject, injectable } from 'tsyringe';
@@ -9,6 +8,7 @@ import User from '@modules/users/infra/typeorm/entities/User';
 import authConfig from '@config/auth';
 
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   email: string;
@@ -25,6 +25,9 @@ class AuthenticateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -34,7 +37,10 @@ class AuthenticateUserService {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
 
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination.', 401);
---
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,49 @@
+import AppError from '@shared/errors/AppError';
+
+import CreateUserService from './CreateUserService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUsers = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUsers.execute({
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUsers = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUsers.execute({
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+      password: '123456',
+    });
+
+    expect(
+      createUsers.execute({
+        email: 'johndoe@example.com',
+        name: 'John Doe',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
